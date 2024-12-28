e16583ff75e19d08596e429c0348cb65297fcc2e
Hannah Hackney <Emilyhackney014@gmail.com>
2024-12-28 04:16:27 -0500

    Adjusts example output

--- RPSGT.Test/src/RPSGT/Test/Gittest.java
+++ RPSGT.Test/src/RPSGT/Test/Gittest.java
@@ -2,6 +2,6 @@ package RPSGT.Test;
 
 public class Gittest {
 	  public static void main(String[] args) {
-		    System.out.println("Git is fun");
+		    System.out.println("Git is cool");
 		  }
 		}
